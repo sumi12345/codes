d4f67cae7eceab729003bd0b220e3117482f880f
sumi12345 <753007687@qq.com>
2018-12-08 14:50:51 +0800

    2015_2_2 kiddie Pool

--- 2015_2_2.php
+++ 2015_2_2.php
@@ -3,6 +3,11 @@ ini_set("max_execution_time","3");
 ini_set("memory_limit","3M");
 
 class Kiddie_pool {
+    public function solve($N, $V, $X, $R, $C) {
+        $r = $this->getTime($N, $V, $X, $R, $C);
+        if ($r === false) return 'IMPOSSIBLE';
+        return number_format($r, 8, '.', '');
+    }
 
     /**
      * @param $N int N个水源
@@ -11,62 +16,48 @@ class Kiddie_pool {
      * @param $R array 各水源的最大流量
      * @param $C array 各水源的温度
      */
-    public function solve($N, $V, $X, $R, $C) {
-        // 如果所有水源都以最大流量放水 1 时间, 得到的体积和温度
-        $temp = $this->calculate_temp($R, $C);
-
-        // 如果水温偏低/偏高 调整最冷/最热水源的流量
-        $t = $temp['t'];
-        $v = $temp['v'];
-        if ($t / $v < $X) asort($C);
-        elseif ($t / $v > $X) arsort($C);
-        if ($t / $v == $X) return $V / $v;
-
-        // 调整流量
-        foreach ($C as $key => $c) {
-            if ($t / $v == $X) break;
-            $this->ajust_temp($X, $t, $v, $R[$key], $c);
+    public function getTime($N, $V, $X, $R, $C) { echo 'solve(V='.$V.', X='.$X.')';
+        // 从冷往热计算体积和温度
+        $cold_r = []; $cold_c = []; asort($C);
+        $this->calculateRC($R, $C, $cold_r, $cold_c);
+        // 从热往冷计算体积和温度
+        $hot_r = []; $hot_c = []; arsort($C);
+        $this->calculateRC($R, $C, $hot_r, $hot_c);
+        // 如果达不到温度
+        if ($X < $cold_c[0] || $X > $hot_c[0]) return false;
+        // 如果最终温度就是所需温度
+        if (''.$X == ''.$cold_c[$N - 1]) return $V / $cold_r[$N - 1];
+        // 如果最终温度太热
+        if ($X <= $cold_c[$N - 1]) { echo ' too hot! ('; echo implode(' ', $cold_c).')';
+            sort($C);
+            for ($i = $N - 1; $i > 0; $i --) {
+                if ($cold_c[$i - 1] > $X) continue;
+                $r = ($X - $cold_c[$i - 1]) * $cold_r[$i - 1] / ($C[$i] - $X);
+                return $V / ($r + $cold_r[$i - 1]);
+            }
         }
-
-        // 没办法达到目标水温
-        if ($v == 0) return 'IMPOSSIBLE';
-
-        // 如果已经达到温度 计算时间
-        return number_format($V / $v, 9, '.', '');
+        // 如果最终温度太冷
+        if ($X >= $hot_c[$N - 1]) { echo ' too cold! ('; echo implode(' ', $hot_c).')';
+            rsort($C);
+            for ($i = $N - 1; $i > 0; $i --) {
+                if ($hot_c[$i - 1] < $X) continue;
+                $r = ($X - $hot_c[$i - 1]) * $hot_r[$i - 1] / ($C[$i] - $X);
+                return $V / ($r + $hot_r[$i - 1]);
+            }
+        }
     }
 
-    // 计算温度
-    private function calculate_temp($R, $C) {
-        $v = 0;
-        $t = 0;
-        foreach ($C as $key => $temp) {
-            $v += $R[$key];
-            $t += $temp * $R[$key];
-        }
-        return array('t' => $t, 'v' => $v);
-    }
-
-    // 调整温度
-    private function ajust_temp($X, &$t, &$v, $R, $C) {
-        if ($X == $C) return true;
-        $t -= $R * $C;
-        $v -= $R;
-        $r = ($t - $X * $v) / ($X - $C);
-        if ($r >= 0) {
-            $t += $r * $C;
-            $v += $r;
+    public function calculateRC($R, $C, &$lr, &$lc) {
+        $p = -1;
+        foreach ($C as $k => $c) {
+            if ($p == -1) { $lc[] = $c; $lr[] = $R[$k]; $p ++; continue; }
+            $lc[] = ($lr[$p] * $lc[$p] + $R[$k] * $c) / ($lr[$p] + $R[$k]);
+            $lr[] = $lr[$p] + $R[$k];
+            $p ++;
         }
     }
 
 }
-$P = new Kiddie_pool();
-echo $P->solve(1, 10, 50, [0.2000], [50.0000])."\n";
-echo $P->solve(2, 30.0000, 65.4321, [0.0001, 100.0000], [50.0000, 99.9000])."\n";
-echo $P->solve(2, 5.0000, 99.9000, [30, 20], [99.8999, 99.7000])."\n";
-echo $P->solve(2, 0.0001, 77.2831, [0.0001, 0.0001], [97.3911, 57.1751])."\n";
-echo $P->solve(2, 100.0000, 75.6127, [70.0263, 27.0364], [75.6127, 27.7990])."\n";
-echo $P->solve(4, 5000.0000, 75.0000, [10, 20, 300, 40], [30, 50, 95, 2])."\n";
-exit;
 
 class Input {
     private $in_file;
@@ -87,12 +78,12 @@ class Input {
                 echo 'Case #'.$c.': ';
                 file_put_contents($this->out_file, 'Case #'.$c.': ', FILE_APPEND);
                 $conf = explode(' ', trim(fgets($handle)));
-                $N = intval($conf[0]); $V = floatval($conf[1]); $X = floatval($conf[2]); $R = array(); $C = array();
-                for ($i = 0; $i < $N; $i ++) {
-                    $row = explode(' ', trim(fgets($handle)));
-                    $R[] = floatval($row[0]); $C[] = floatval($row[1]);
+                $R = array(); $C = array();
+                for ($i = 0; $i < $conf[0]; $i ++) {
+                    $r = explode(' ', trim(fgets($handle)));
+                    $R[] = $r[0]; $C[] = $r[1];
                 }
-                $r = $P->solve($N, $V, $X, $R, $C);
+                $r = $P->solve($conf[0], $conf[1], $conf[2], $R, $C);
                 echo ($r)."\n";
                 file_put_contents($this->out_file, ($r).($c == $cases ? "" : "\r\n"), FILE_APPEND);
             }
@@ -102,7 +93,9 @@ class Input {
 }
 
 $t = time();
-// $i = new Input('../下载/B-small-practice.in','../下载/OUT_2.txt');
+//$i = new Input('../下载/IN.txt','../下载/OUT_2.txt');
+//$i = new Input('../下载/B-small-practice.in','../下载/OUT_2.txt');
+$i = new Input('../下载/B-large-practice.in','../下载/OUT_2.txt');
 $i->process();
 
 echo "\n".'execution time: '.(time() - $t);
@@ -120,4 +113,12 @@ echo "\n".'memory peak usage: '.(memory_get_peak_usage() / 1024 / 1024);
  * 我们先将所有水源都放到最大流量. 如果温度正好是我们要的温度, 问题解决.
  * 如果温度太高, 我们要降低某些水源的流速, 为了让流量的减少最少, 我们从最热的水源开始减.
  * 如果达不到理想温度, 那就不可能解决. 太冷的解决方案也是一样的.
+ *
+ * pitfalls:
+ * 思路是, 如果最终结果太热, 将水源温度升序排列
+ * 如果前 i - 1 个温度不太热了, 保留所有前 i - 1 个, 第 i 个视所需体积而定.
+ * 所以, N = 1 时, 没有第 i - 1 个, 就不会进入循环.
+ * $r = ($X - $cold_c[$i - 1]) * $cold_r[$i - 1] / ($C[$i] - $X); 这里 C[i] - X 可能为 0
+ * 小数据集 case #41, X=5.9770, 水源温度为 5.9770, 5.9770, 但是 X == cold_r[N - 1] 为假
+ * 只好改为 ''.$X 和 ''.$cold_r[$N - 1] 转换为字符串再比较
  */

9c07bab1b88ef86e1fa68aaf94666567d4aca131
sumi12345 <753007687@qq.com>
2018-12-11 17:02:18 +0800

    2015_1A_3 Logging

--- 2015_1A_3.php
+++ 2015_1A_3.php
@@ -1,4 +1,123 @@
 <?php
+ini_set("max_execution_time","60");
+ini_set("memory_limit","60M");
+
+class Logging {
+    public function solve($N, $P) { echo 'solve: '.$N."\n";
+        $list = [];     // 最终的列表
+
+        if ($N <= 3) {  // 如果树的数量 < 3 不用砍
+            for ($i = 0; $i < $N; $i ++) $list[] = 0;
+            return "\n".implode("\n", $list);
+        }
+        /* if ($N <= 15) { // 小数据集
+            for ($i = 0; $i < $N; $i ++) $list[] = $this->solve_small($N, $P, $i);
+            return "\n".implode("\n", $list);
+        } */
+        if ($N <= 3000) { // 大数据集
+            for ($i = 0; $i < $N; $i ++) $list[] = $this->solve_large($N, $P, $i);
+            return "\n".implode("\n", $list);
+        }
+    }
+
+    private function solve_large($N, $P, $i) { // echo 'i = '.$i.'--------------------';
+        // 记录树 i 到每个点的角度
+        $S = [];
+        for ($j = 0; $j < $N; $j ++) {
+            if ($j == $i) continue;
+            $x = $P[$j][0] - $P[$i][0]; $y = $P[$j][1] - $P[$i][1];
+            if ($x == 0) $a = $y > 0 ? pi() / 2 : - pi() / 2; // 90 度, -90 度
+            elseif ($y == 0) $a = $x > 0 ? 0 : pi();          // 0 度, 180 度
+            else {
+                $a = atan($y / $x);
+                if ($a > 0 && $y < 0) $a -= pi();            // atan 为正, 但 x < 0, y < 0
+                if ($a < 0 && $y > 0) $a += pi();            // atan 为负, 但 y > 0, x < 0
+            }
+            $S[$j] = $a / pi() * 180 * 1000000;              // 转换为 角度 x 10^6
+        }
+        sort($S); // print_r($S);
+        // 计算至少需要砍多少树
+        $min_log = $N; $head = 0;
+        for ($tail = 0; $tail < $N - 1; $tail ++) {  // 尾指针每次进 1
+            if ($head == $tail) $head = $this->moveHead($head, 1, $N - 1); // 一开始头尾指针相等
+            for (; $this->angleDiff($S, $tail, $head) < 180 * 1000000 && $head != $tail; $head = $this->moveHead($head, 1, $N - 1));
+            $head = $this->moveHead($head, -1, $N - 1);   // 找到这条线左边的最后一个点
+            if ($this->angleDiff($S, $tail, $head) >= 180 * 1000000) $log = 0;  // 左边没有点, 方向相同也算, 所以设相同为 360 度
+            else { $log = $head - $tail; if ($log < 0) $log += $N - 1; } // 需要砍多少树, 如果窗口为负数, 加上 N - 1
+            if ($log < $min_log) $min_log = $log; // echo 'log (i='.$i.', tail=' .$tail.', head='.$head.') ='.$log."\n";
+        }
+        return $min_log;
+    }
+
+    // 移动头指针
+    private function moveHead($head, $d, $n) {  // if ($d == -1) echo 'found: '.$head.', '.$d;
+        $head = $head + $d;
+        if ($head > $n - 1) $head -= $n;
+        if ($head < 0) $head += $n;             // if ($d == -1) echo ' = '.$head."\n";
+        return $head;
+    }
+
+    // 计算从 a1 到 a2 经过多少角度
+    private function angleDiff(&$S, $i, $j) {
+        $diff = $S[$j] - $S[$i];
+        if ($diff <= 0) $diff += 360 * 1000000; // echo 'angleDiff: ('.$i.', '.$j.') = '.($diff / 1000000)."\n";
+        return $diff;
+    }
+
+    // 小数据集的解决方法
+    private function solve_small($N, $P, $i) { // 对于编号为 i 的树
+        $min_log = $N;  // 至少要砍多少棵
+        for ($j = 0; $j < $N; $j ++) {         // 沿着 ij 方向看过去
+            $log = 0;
+            if ($j == $i) continue;
+            for ($k = 0; $k < $N; $k ++) {     // 有多少棵树在 ij 的左边
+                if ($k == $i || $k == $j) continue;
+                $x1 = $P[$j][0] - $P[$i][0]; $y1 = $P[$j][1] - $P[$i][1];  // ij 方向
+                $x2 = $P[$k][0] - $P[$i][0]; $y2 = $P[$k][1] - $P[$i][1];  // ik 方向
+                if ($x1 * $y2 - $x2 * $y1 < 0) $log ++;
+            }
+            if ($log < $min_log) $min_log = $log;
+        }
+        return $min_log;
+    }
+}
+
+class Input {
+    private $in_file;
+    private $out_file;
+
+    function __construct($i, $o) {
+        $this->in_file = $i;
+        $this->out_file = $o;
+    }
+
+    function process() {
+        $handle = fopen($this->in_file, "r");
+        if ($handle) {
+            $cases = intval(fgets($handle, 32));
+            file_put_contents($this->out_file, '');
+            for($c = 1; $c <= $cases; $c ++) {
+                echo 'Case #'.$c.': ';
+                file_put_contents($this->out_file, 'Case #'.$c.': ', FILE_APPEND);
+                $N = trim(fgets($handle)); $P = [];
+                for ($i = 0; $i < $N; $i ++) $P[] = explode(' ', trim(fgets($handle)));
+                $L = new Logging();
+                $r = $L->solve($N, $P);
+                echo ($r)."\n";
+                file_put_contents($this->out_file, ($r).($c == $cases ? "" : "\r\n"), FILE_APPEND);
+            }
+            fclose($handle);
+        }
+    }
+}
+$t = time();
+//$i = new Input('../下载/C-small-practice.in','../下载/OUT_3.txt');
+$i = new Input('../下载/C-large-practice.in','../下载/OUT_3.txt');
+$i->process();
+
+echo "\n".'execution time: '.(time() - $t);
+echo "\n".'memory peak usage: '.(memory_get_peak_usage() / 1024 / 1024);
+
 /**
  * Created by PhpStorm.
  * User: sumi

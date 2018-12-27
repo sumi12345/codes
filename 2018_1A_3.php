78a427ff189456b55ef50a96ea7540f2137a25d9
sumi12345 <753007687@qq.com>
2018-12-27 18:12:31 +0800

    2018_1A_3 edgy baking large failed

--- 2018_1A_3.php
+++ 2018_1A_3.php
@@ -1,4 +1,5 @@
 <?php
+ini_set("memory_limit", "128M");
 define('ENV', 'test');
 
 function solve($N, $P, $W, $H) {
@@ -21,27 +22,28 @@ function solve_large($N, $P, $W, $H) {
     for ($i = 0; $i < $N; $i ++) $h += $high[$i];
     if ($sum + $h < $P) return $sum + $h;
     // if P is reachable, find a subset
-    $DP = [];
-    return $sum + subset($N - 1, $low, $high, $P - $sum, $P - $sum, $DP);
+    $str = ''; $DP = [];
+    $r = $sum + nextItem($str, $P - $sum, $low, $high, $DP); dd($P - $sum, 'P - sum'); dd($DP, 'DP');
+    return $r;
 }
 
-function subset($I, $low, $high, $L, $H, &$DP) {
-    // logged
-    if (isset($DP[$I][$L][$H])) return $DP[$I][$L][$H];
-    // hit, go with high bound
-    if ($low[$I] <= $L && $high[$I] >= $H) return $H;
-    // one left and not hit
-    if ($I == 0) return $low[$I] > $L ? 0 : $high[$I];
-    // without & with this item
-    $s1 = subset($I - 1, $low, $high, $L, $H, $DP);
-    $s2 = $low[$I] > $L ? 0 : subset($I - 1, $low, $high, $L - $low[$I], $H - $high[$I], $DP) + $high[$I];
-    dd('subset: I='.$I.', L='.$L.', H='.$H.', low[I]='.$low[$I].', high[I]='.$high[$I].', s1='.$s1.', s2='.$s2);
-    // return
-    $r = $L - $s1 > $L - $s2 ? $s2 : $s1;
-    if (!isset($DP[$I])) $DP[$I] = [];
-    if (!isset($DP[$I][$L])) $DP[$I][$L] = [];
-    $DP[$I][$L][$H] = $r;
-    return $r;
+function nextItem($str, $p, $low, $high, &$DP) {
+    // check the sum of lower and higher bound
+    $lower_sum = 0; $higher_sum = 0; $len = strlen($str);
+    for ($i = 0; $i < $len; $i ++) if ($str[$i] == '1') {
+        $lower_sum += $low[$i]; $higher_sum += $high[$i];
+    }
+    // reach the max digit
+    if ($len == count($low)) { $DP[$str] = min($higher_sum, $p); return $DP[$str]; }
+    // no necessary to continue
+    $l = $low[$len]; $h = $high[$len]; dd($l.', '.$h, 'range of next digit of '.$str);
+    if ($lower_sum + $l > $p) { $DP[$str] = min($higher_sum, $p); dd($str, 'no more'); return $DP[$str]; }  // no more
+    if ($higher_sum + $h >= $p) { $DP[$str] = $p; dd($str, 'hit'); return $p; } // hit at this digit
+    // check next digit
+    $r1 = nextItem($str.'1', $p, $low, $high, $DP);     // with
+    if ($r1 == $p) { $DP[$str] = $p; dd($str, 'with'); return $p; }
+    $r2 = nextItem($str.'0', $p, $low, $high, $DP);     // without
+    $DP[$str] = max($r1, $r2); dd($str, 'with or without'); return $DP[$str];
 }
 
 function solve_small($N, $P, $W, $H) {
@@ -62,11 +64,11 @@ function fake() {
     $N = rand(1, 100); $W = []; $H = []; $sum = 0; $diag_sum = 0;
     for ($i = 0; $i < $N; $i ++) {
         $w = rand(1, 250); $h = rand(1, 250);
-        //if ($i > 0) { $w = $W[$i - 1]; $h = $H[$i - 1]; }
+        if ($i > 0) { $w = $W[$i - 1]; $h = $H[$i - 1]; }
         $W[] = $w; $H[] = $h;
         $sum += 2 * $w + 2 * $h; $diag_sum += 2 * sqrt($w * $w + $h * $h);
     }
-    $P = rand($sum, $sum + floor($diag_sum));
+    $P = rand($sum, $sum + floor($diag_sum / 2));
     $in = $N.' '.$P."\n";
     for ($i = 0; $i < $N; $i ++) $in .= $W[$i].' '.$H[$i]."\n";
     file_put_contents('../下载/IN.txt', $in, FILE_APPEND);
@@ -98,7 +100,7 @@ if (ENV == 'test') {
 }
 
 //---- start process ----
-file_put_contents('../下载/IN.txt', "100\n"); for ($i = 0; $i < 100; $i ++) fake();
+//file_put_contents('../下载/IN.txt', "100\n"); for ($i = 0; $i < 100; $i ++) fake();
 $T = read($hr);
 for ($c = 1; $c <= $T; $c ++) {
     write('Case #'.$c.': ', $hw);
@@ -107,7 +109,7 @@ for ($c = 1; $c <= $T; $c ++) {
     for ($i = 0; $i < $conf[0]; $i ++) {
         $r = explode(' ', read($hr));
         $W[] = $r[0]; $H[] = $r[1];
-    }
+    } //if ($c != 4) continue;
     write(solve($conf[0], $conf[1], $W, $H)."\n", $hw);
 }
 /**

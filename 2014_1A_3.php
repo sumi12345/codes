ca80406420f50f977258f8df689395bbebcabf35
sumi12345 <753007687@qq.com>
2019-02-27 16:09:45 +0800

    2014_1A_3 proper shuffle

--- 2014_1A_3.php
+++ 2014_1A_3.php
@@ -2,59 +2,63 @@
 define('ENV', 'test');
 
 function solve($S) {
-    $R = array_pop($S);  // fake last one is result
     $N = count($S);
 
-    // calculate P(S|BAD)
-    $pmove = 1 / $N;
-    $pstay = 1 - $pmove;
-
-    $prev = [];
-    for ($i = 0; $i < $N; $i ++) {
-        for ($j = 0; $j < $N; $j ++) {
-            $prev[$i][$j] = $i == $j ? 1 : 0;
-        }
-    }
-
-    // swap number at previous kth position
-    // number at previous kth position will split to every position
-    // number not at previous kth position
-    // will have 1/N chance to move to k, 1-1/N stay where it is
-    // next[i][k] = sum(1/N * prev[][])
-    $next = [];
-    for ($k = 0; $k < $N; $k ++) {    // swap num at prev kth position
-        for ($i = 0; $i < $N; $i ++) {      // number
-            $next[$i] = []; $next[$i][$k] = 0;
-            for ($j = 0; $j < $N; $j ++) {  // previous position
-                $next[$i][$k] += $prev[$i][$j] * $pmove;  // others move to k or k stay
-                if ($j != $k) $next[$i][$j] = $prev[$i][$j] * $pstay  // others stay
-                                            + $prev[$i][$k] * $pmove; // k move to others
-            }
-        }
-        //dd($next[$N - 1], $k);
-        $prev = $next;
-    }
+    // calculate P_BAD
+    Bad::swap($N);
+    $next = Bad::$next;
 
     // return
     $P_BAD = 1;
     foreach ($S as $k => $v) $P_BAD *= $next[$v][$k];
     $P_GOOD = 1 / pow($N, $N);
-    $P = $P_GOOD / ($P_GOOD + $P_BAD);
-    $r = $P > 0.5 ? 'GOOD' : 'BAD';
-    dd($P.', '.$P_BAD, 'P, P_BAD');
-    dd($R.'|'.$r, 'result');
+    $r = $P_GOOD > $P_BAD ? 'GOOD' : 'BAD';
+    dd($P_GOOD.', '.$P_BAD, 'P_GOOD, P_BAD');
     return $r;
 }
 
-function factorial($N) {
-    if ($N <= 1) return 1;
-    $r = $N * factorial($N - 1);
-    //dd($r, 'factorial '.$N);
-    return $r;
+class Bad {
+    static $next;
+
+    static public function swap($N) {
+        // set only once with N
+        if (!empty(self::$next)) return;
+
+        // calculate P(S|BAD)
+        dd('calculate P(S|BAD)', 'Bad::swap('.$N.')');
+        $pmove = 1 / $N;
+        $pstay = 1 - $pmove;
+
+        $prev = [];
+        for ($i = 0; $i < $N; $i ++) {
+            for ($j = 0; $j < $N; $j ++) {
+                $prev[$i][$j] = $i == $j ? 1 : 0;
+            }
+        }
+
+        // swap number at previous kth position
+        // number at previous kth position will split to every position
+        // number not at previous kth position
+        // will have 1/N chance to move to k, 1-1/N stay where it is
+        $next = [];
+        for ($k = 0; $k < $N; $k ++) {    // swap num at prev kth position
+            for ($i = 0; $i < $N; $i ++) {      // number
+                $next[$i] = []; $next[$i][$k] = 0;
+                for ($j = 0; $j < $N; $j ++) {  // previous position
+                    $next[$i][$k] += $prev[$i][$j] * $pmove;  // others move to k or k stay
+                    if ($j != $k) $next[$i][$j] = $prev[$i][$j] * $pstay  // others stay
+                        + $prev[$i][$k] * $pmove; // k move to others
+                }
+            }
+            //dd($next[$N - 1], $k);
+            $prev = $next;
+        }
+        self::$next = $next;
+    }
 }
 
 function fake() {
-    $N = 3;
+    $N = 125;
     $S = [];
     for ($k = 0; $k < $N; $k ++) $S[$k] = $k;
     if (rand(0, 1)) {
@@ -70,12 +74,12 @@ function fake() {
         }
         $S[] = 'BAD';
     }
-    $str = implode(' ', $S)."\n";
+    $str = $N."\n".implode(' ', $S)."\n";
     file_put_contents('../下载/IN.txt', $str, FILE_APPEND);
 }
 
 function dd($item, $name = '') {
-    if (ENV != 'test') return;
+    // if (ENV != 'test') return;
     if ($name != '') echo $name.': ';
     if (is_array($item)) print_r($item);
     else echo $item."\n";
@@ -95,18 +99,26 @@ if (ENV == 'test') {
     $hw = fopen('../下载/OUT_3.txt', 'w');
     $hw = fopen('../下载/OUT_3.txt', 'a');
 } else {
-    $hr = STDIN;
-    $hw = STDOUT;
+    $hr = fopen('../下载/C-small-practice.in', 'r');
+    $hw = fopen('../下载/OUT_3.txt', 'w');
+    $hw = fopen('../下载/OUT_3.txt', 'a');
 }
 
 //---- start process ----
 file_put_contents('../下载/IN.txt', "99\n"); for ($i = 0; $i < 99; $i ++) fake();
 $T = read($hr);
+$correct = 0;
 for ($c = 1; $c <= $T; $c ++) {
     write('Case #'.$c.': ', $hw);
+    $N = read($hr);
+    $S = explode(' ', read($hr));
+    $R = ENV == 'test' ? array_pop($S) : 'GOOD';
     //if ($c != 1) continue;
-    write(solve(explode(' ', read($hr)))."\n", $hw);
+    $r = solve($S);
+    if ($R == $r) $correct ++;
+    write($r."\n", $hw);
 }
+if (ENV == 'test') dd($correct.'/'.$T, 'correct / total');
 /**
  * Created by PhpStorm.
  * User: sumi
@@ -115,4 +127,6 @@ for ($c = 1; $c <= $T; $c ++) {
  *
  * 问题是, 计算出来的概率并不是全排列 (01, 10), 而是 (00, 01, 10, 11),
  * 会造成 1/N! 远大于这些相乘的概率, 如果换成 1/(N^N), 准确率又很低.
+ * 测试案例中 N=1000, 计算精度不允许, 会造成 P_GOOD 和 P_BAD 都是 0
+ * 事实上 N=150 就已经不支持精度了, N=125 时正确率大概在 70%
  */

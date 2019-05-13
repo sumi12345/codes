39f318eb64a25a9eee53ea61b24d2025be4a4d38
sumi12345 <753007687@qq.com>
2019-05-13 16:46:30 +0800

    2016_1C_3 fashion police small

--- 2016_1C_3.php
+++ 2016_1C_3.php
@@ -9,21 +9,57 @@ class FashionPolice {
         for ($i = 0; $i < $num; $i ++) {
             $j = floor($i / $numP / $P) % $J + 1; // numP * P 个 J
             $p = floor($i / $numP) % $P + 1;      // min(K, J * S)个1, 2, 3
-            $s = ($P == $S && $K == 1) ? ($j + $p) % $S + 1 : $i % $S + 1;
+            $s = $i % $S + 1;
+            if ($K < $S && $P == $S && $K == 1) $s = ($j + $p) % $S + 1;  // 2, 2, 2, 1
+            if ($K < $S && $P == $S && $K == 2) $s = ($j + $p + $i * 2) % $S + 1;  // 3, 3, 3, 2
             $C[$i] = [$j, $p, $s];
         }
 
-        $str = $num."\n";
+        $str = '';
         for ($i = 0; $i < $num; $i ++) $str .= implode(' ', $C[$i])."\n";
-        return trim($str);
+        $str = trim($str);
+        $r = $this->check($str, $K);
+        return $num."\n".$str;
+    }
+
+    private function check($str, $K) {
+        // 检查全套组合重复
+        $set = [];
+        $three = explode("\n", $str);
+        foreach ($three as $t) {
+            if (isset($set[$t])) { echo $t; return -1; }
+            $set[$t] = 1;
+        }
+        // 检查 JS 组合重复 K 次以上
+        $set = [];
+        $three = explode("\n", $str);
+        foreach ($three as $t) {
+            $row = explode(' ', $t);
+            $js = $row[0].' '.$row[2];
+            if (!isset($set[$js])) $set[$js] = 0;
+            $set[$js] ++;
+            if ($set[$js] > $K) { echo $js; return -2; }
+        }
+        // 检查 PS 组合重复 K 次以上
+        $set = [];
+        $three = explode("\n", $str);
+        foreach ($three as $t) {
+            $row = explode(' ', $t);
+            $ps = $row[1].' '.$row[2];
+            if (!isset($set[$ps])) $set[$ps] = 0;
+            $set[$ps] ++;
+            if ($set[$ps] > $K) { echo $ps; return -3; }
+        }
+
+        return 1;
     }
 
 }
 
 $F = new FashionPolice();
-echo $F->solve(1, 2, 3, 2);
 echo $F->solve(2, 2, 2, 1);
-exit;
+echo $F->solve(3, 3, 3, 2);
+//exit;
 
 class Input {
     private $in_file;
@@ -55,7 +91,7 @@ class Input {
 
 $t = time();
 
-//$i = new Input('../下载/C-small-practice.in','../下载/OUT_3.txt');
+$i = new Input('../下载/C-small-practice.in','../下载/OUT_3.txt');
 //$i = new Input('../下载/C-large-practice.in','../下载/OUT_3.txt');
 $i->process();
 
@@ -71,7 +107,6 @@ echo "\n".'memory peak usage: '.(memory_get_peak_usage() / 1024 / 1024);
  * 如果你两天穿同一套套装, 或者其中两个单品的组合穿了超过K天, 就会被抓去坐牢!
  * 求你带的衣服可以撑多少天不被抓走, 并给出组合
  *
- * 首先, 最多可以有 J * P * min(S, K) 种组合, 所以 J * P 都要用上, 每组重复 min(S, K) 次
- * 第三位原来想就123123循环, 但发现 (2, 2, 2, 1) 的 case, P 和 S 会同步循环
- * 修正了 P = S 且 K = 1 的情况后还是不能通过, 原因不明
+ * 最多有 J * P * min(S, K) 种组合, 所以 J * P 都要用上, 每组重复 min(S, K) 次
+ * 特殊处理 (2, 2, 2, 1) 和 (3, 3, 3, 2)
  */
